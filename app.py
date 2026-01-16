2038a87b0bc5328f39ec514b2b0a66b5ec5578a9
Vatsal Upadhyay <vatsal11042009@gmail.com>
2026-01-15 20:30:17 -0500

    Connect dashboard route to HTML template

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 # myPlanner Flask app
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def dashboard():
-    return "Welcome to myPlanner"
+    return render_template("dashboard.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
