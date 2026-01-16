fbea1d9d3ef44a1d3a43a1a790c4d0c086005b67
Vatsal Upadhyay <vatsal11042009@gmail.com>
2026-01-15 20:26:03 -0500

    Server setup

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 # myPlanner Flask app
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def dashboard():
+    return "Welcome to myPlanner"
+
+if __name__ == "__main__":
+    app.run(debug=True)
